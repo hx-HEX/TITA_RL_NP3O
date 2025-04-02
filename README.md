5078dc70443ea9f123f0114dea7d6942345bdaf7
hx-HEX <1499217911@qq.com>
2025-04-02 15:31:19 +0800

    本末开源rl改，添加上楼梯奖励，更改指令

---
+++ README.md
@@ -0,0 +1,200 @@
+# TITA_RL_NP3O
+本末开源RL改
+## 0. 指引
+
+>每个人的环境都不一样，遇到问题可以查看maybe_problems.md文件或在Issues上反馈。
+>
+>持续更新中
+>
+>English README.md：to be updated
+
+
+![alt text](pictures_videos/output.gif)  
+
+本仓库强化学习部分基于：
+
+[N3PO Locomoton](https://github.com/zeonsunlightyu/LocomotionWithNP3O.git)
+
+另附titatit四足模式训练环境：
+
+[TITATIT-Quadruped Mode](https://github.com/DDTRobot/titatit_rl)
+
+**参考环境**
+
+| Environment        | Brief info   |
+| --------   | ----- | 
+| 显卡| RTX 3060 |
+| CUDA | CUDA12.5 |
+| 训练环境 | isaacgym |
+| sim2sim| Webots2023 |
+| ROS | ROS2 Humble |
+| 推理 | RTX 3060 / Jetson Orin NX on TITA + tensorRT|
+| 虚拟环境 | anaconda |
+
+
+
+### 本次开源包含有三部分  
+
+#### Isaac Gym仿真训练  
+
+![alt text](<pictures_videos/isaac_gym.gif>)
+    
+#### sim2sim仿真  
+        
+[tita_rl_sim2sim2real](https://github.com/DDTRobot/tita_rl_sim2sim2real)
+
+![alt text](<pictures_videos/sim_webots.gif>)
+#### sim2real实机部署
+
+[tita_rl_sim2sim2real](https://github.com/DDTRobot/tita_rl_sim2sim2real)
+
+![alt text](pictures_videos/real_robot.gif)
+
+## 1. 环境搭建
+>如您已有配好的RL环境，请直接跳至第3节开始训练
+
+#### 1.1 安装NVIDIA显卡驱动
+
+**方式1：使用ubuntu软件中心安装驱动**
+>http://www.nvidia.cn/Download/index.aspx?lang=cn
+
+
+**方式2：端中使用apt工具包安装**
+
+添加 PPA 源：  
+```markdown
+    sudo add-apt-repository ppa:graphics-drivers/ppa  
+``` 
+为系统安装依赖项以构建内核模块： 
+```bash 
+sudo apt-get install dkms build-essential  
+```  
+安装NVIDIA驱动  
+```bash 
+sudo ubuntu-drivers autoinstall  
+```
+系统会自动安装推荐版本驱动，安装完重启系统  
+```bash 
+sudo reboot  
+```
+
+#### 1.2 安装anaconda  
+[anaconda-installation](https://www.anaconda.com/download/success)  
+
+#### 1.3 安装cuda
+[cuda-toolkit-installation](https://developer.nvidia.com/cuda-toolkit-archive)
+
+使用以下指令检查是否成功安装
+
+```bash
+nvidia-smi
+```
+
+#### 1.4. 安装tenssorrt  
+我的cuda版本是12.0,所以我安装tensorrt8.6.0  
+[tensorRT-installation](https://developer.nvidia.com/nvidia-tensorrt-8x-download)
+
+#### 1.5. 安装issacgym  
+[isaacgym-installation](https://developer.nvidia.com/isaac-gym/download)  
+
+
+#### 1.5. 安装rsl_rl (PPO implementation)  
+Clone https://github.com/leggedrobotics/rsl_rl  
+ `cd rsl_rl && git checkout v1.0.2 && pip install -e .` 
+
+## 2. 测试环境
+
+>注意不要照抄指令
+>
+><your_env_name>为你的虚拟环境名
+>
+><your_path>为对应文件路径  
+
+
+#### 2.1. conda配置虚拟环境
+```bash
+conda create -n <your_env_name> python=3.8
+```
+<your_env_name>为你的虚拟环境名该环境配置，在你的anaconda安装路径<your_path>/anaconda3/envs能找到<your_env_name>这个虚拟环境  
+#### 2.2. 激活环境
+```bash
+conda activate <your_env_name>
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:<your_path>/anaconda3/envs/<your_env_name>/lib
+```
+能在终端开头看到<your_env_name>,说明激活成功
+
+#### 2.3. 测试conda和issacgym是否安装成功
+安装以下包
+```bash
+pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+```
+进入isaacgym安装路径
+```bash
+cd 你的路径/isaacgym/python && pip install -e .  
+```
+测试
+```bash
+cd examples && python 1080_balls_of_solitude.py
+```
+看到一堆球落到地上表示安装成功，若没有参考第4步的解决方法
+
+#### 2.4. 可能遇到的问题，“Isaac Gym”没有反应,运行以下两个指令有其它问题查看maybe_problems.md
+```bash
+sudo prime-select nvidia
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+```
+
+#### 2.5. 退出conda环境
+```bash
+conda deactivate
+```
+
+## 3. 开始训练
+
+#### 3.1. 从github上下载代码
+```bash
+git clone https://github.com/DDTRobot/tita_rl.git
+cd tita_rl
+```
+#### 3.2. 激活conda环境   
+```bash 
+conda activate <your_env_name>
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:your path/anaconda3/envs/<your_env_name>/lib
+```
+#### 3.3. 运行训练程序
+```bash
+python train.py --task=tita_constraint 
+```
+显存不够会非常卡，看到如下图片，表示程序正常执行，ctrl+c退出
+
+![alt text](pictures_videos/image-1.png)
+    
+测试使用的是NVIDIA GeForce RTX 3060，打开图形界面的话，会非常卡，建议关闭图形界面
+    
+![alt text](pictures_videos/image-2.png)\
+    
+为了解决显存不足卡顿的问题，我们可以使用--headless参数，这样程序会以命令行的形式运行，不会打开图形界面，这样可以节省显存，提高运行速度
+
+```bash
+python train.py --task=tita_constraint --headless
+```
+
+![alt text](pictures_videos/image-3.png)  
+     
+![alt text](pictures_videos/c7f9d78b-e6f7-46a5-b9cc-187ca142d9f5.jpeg)
+
+## 4. 测试训练成果
+#### 4.1. 查看训练成果
+训练好的文件在tita_rl/logs下，例如model_10000.pt，将它拷贝到tita_rl主目录下，然后运行能看到
+```bash
+python simple_play.py --task=tita_constraint
+```
+![alt text](<pictures_videos/isaac_gym.gif>)
+#### 4.2. 将tita_rl主目录下的test.onnx推理转成model_gn.engine做sim2sim仿真
+```bash
+/usr/src/tensorrt/bin/trtexec --onnx=test.onnx --saveEngine=model_gn.engine
+```
+至此，iaacgym仿真和推理部分已经完成，接下来转到sim2sim和sim2real部分。  
+
+sim2sim2real参考：[tita_rl_sim2sim2real](https://github.com/DDTRobot/tita_rl_sim2sim2real)
+
---
+++ utils/task_registry.py
@@ -0,0 +1,186 @@
+import os
+from datetime import datetime
+from typing import Tuple
+import torch
+import numpy as np
+from shutil import copyfile
+import ntpath
+
+from envs.vec_env import VecEnv
+from runner import OnConstraintPolicyRunner
+
+from global_config import ROOT_DIR, ENVS_DIR
+from .helpers import get_args, update_cfg_from_args, class_to_dict, get_load_path, set_seed, parse_sim_params
+from configs import LeggedRobotCfg, LeggedRobotCfgPPO
+
+from runner import OnPolicyRunner
+
+class TaskRegistry():
+    def __init__(self):
+        self.task_classes = {}
+        self.env_cfgs = {}
+        self.train_cfgs = {}
+    
+    def register(self, name: str, task_class: VecEnv, env_cfg: LeggedRobotCfg, train_cfg: LeggedRobotCfgPPO):
+        self.task_classes[name] = task_class
+        self.env_cfgs[name] = env_cfg
+        self.train_cfgs[name] = train_cfg
+    
+    def get_task_class(self, name: str) -> VecEnv:
+        return self.task_classes[name]
+    
+    def get_cfgs(self, name) -> Tuple[LeggedRobotCfg, LeggedRobotCfgPPO]:
+        train_cfg = self.train_cfgs[name]
+        env_cfg = self.env_cfgs[name]
+        # copy seed
+        env_cfg.seed = train_cfg.seed
+        return env_cfg, train_cfg
+
+
+    def save_cfgs(self, name, train_cfg):
+        """
+        Save all task-related configuration files to the log directory.
+
+        Args:
+            name (str): Task name used to locate the configuration files.
+            train_cfg (object): Training configuration object, used to determine which files to save.
+        """
+        # Ensure the log directory exists
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir, exist_ok=True)
+
+        # Determine the correct Python file based on the runner class name
+        if train_cfg.runner.runner_class_name == "OnPolicyRunner":
+            robot_file = "no_constrains_legged_robot.py"
+        else:
+            robot_file = "legged_robot.py"
+
+        # Define the file paths to be saved (from save_config_files logic)
+        save_items = [
+            os.path.join(ENVS_DIR, robot_file),  # Path to the selected robot file
+            os.path.join(ROOT_DIR, "configs", "legged_robot_config.py"),  # Path to legged_robot_config.py
+            os.path.join(ROOT_DIR, "configs", f"{name}_config.py"),  # Path to task-specific constraint config file
+        ]
+
+        # Add the task-specific Python file path (if it exists)
+        py_root = os.path.join(ENVS_DIR, f"{name}.py")
+        if os.path.exists(py_root):
+            save_items.append(py_root)
+
+        # Additional files to save (from original save_cfgs logic)
+        additional_items = [
+            os.path.join(ROOT_DIR, "configs", f"{name}_config.py"),  # Task-specific constraint config
+        ]
+        save_items.extend(additional_items)
+
+        # Save all files
+        for save_item in save_items:
+            if os.path.exists(save_item):  # Check if the file exists
+                base_file_name = ntpath.basename(save_item)  # Get the file name
+                destination_path = os.path.join(self.log_dir, base_file_name)  # Destination path
+                copyfile(save_item, destination_path)  # Copy the file
+                print(f"Saved: {destination_path}")
+            else:
+                print(f"Warning: {save_item} does not exist and will not be copied.")
+
+
+
+    def make_env(self, name, args=None, env_cfg=None) -> Tuple[VecEnv, LeggedRobotCfg]:
+        """ Creates an environment either from a registered namme or from the provided config file.
+
+        Args:
+            name (string): Name of a registered env.
+            args (Args, optional): Isaac Gym comand line arguments. If None get_args() will be called. Defaults to None.
+            env_cfg (Dict, optional): Environment config file used to override the registered config. Defaults to None.
+
+        Raises:
+            ValueError: Error if no registered env corresponds to 'name' 
+
+        Returns:
+            isaacgym.VecTaskPython: The created environment
+            Dict: the corresponding config file
+        """
+        # if no args passed get command line arguments
+        if args is None:
+            args = get_args()
+        # check if there is a registered env with that name
+        if name in self.task_classes:
+            task_class = self.get_task_class(name)
+        else:
+            raise ValueError(f"Task with name: {name} was not registered")
+        if env_cfg is None:
+            # load config files
+            env_cfg, _ = self.get_cfgs(name)
+        # override cfg from args (if specified)
+        env_cfg, _ = update_cfg_from_args(env_cfg, None, args)
+        set_seed(env_cfg.seed)
+        # parse sim params (convert to dict first)
+        sim_params = {"sim": class_to_dict(env_cfg.sim)}
+        sim_params = parse_sim_params(args, sim_params)
+        env = task_class(   cfg=env_cfg,
+                            sim_params=sim_params,
+                            physics_engine=args.physics_engine,
+                            sim_device=args.sim_device,
+                            headless=args.headless)
+        return env, env_cfg
+
+    def make_alg_runner(self, env, name=None, args=None, train_cfg=None, log_root="default") -> Tuple[OnConstraintPolicyRunner, LeggedRobotCfgPPO]:
+        """ Creates the training algorithm  either from a registered namme or from the provided config file.
+
+        Args:
+            env (isaacgym.VecTaskPython): The environment to train (TODO: remove from within the algorithm)
+            name (string, optional): Name of a registered env. If None, the config file will be used instead. Defaults to None.
+            args (Args, optional): Isaac Gym comand line arguments. If None get_args() will be called. Defaults to None.
+            train_cfg (Dict, optional): Training config file. If None 'name' will be used to get the config file. Defaults to None.
+            log_root (str, optional): Logging directory for Tensorboard. Set to 'None' to avoid logging (at test time for example). 
+                                      Logs will be saved in <log_root>/<date_time>_<run_name>. Defaults to "default"=<path_to_LEGGED_GYM>/logs/<experiment_name>.
+
+        Raises:
+            ValueError: Error if neither 'name' or 'train_cfg' are provided
+            Warning: If both 'name' or 'train_cfg' are provided 'name' is ignored
+
+        Returns:
+            PPO: The created algorithm
+            Dict: the corresponding config file
+        """
+        # if no args passed get command line arguments
+        if args is None:
+            args = get_args()
+        # if config files are passed use them, otherwise load from the name
+        if train_cfg is None:
+            if name is None:
+                raise ValueError("Either 'name' or 'train_cfg' must be not None")
+            # load config files
+            _, train_cfg = self.get_cfgs(name)
+        else:
+            if name is not None:
+                print(f"'train_cfg' provided -> Ignoring 'name={name}'")
+        # override cfg from args (if specified)
+        _, train_cfg = update_cfg_from_args(None, train_cfg, args)
+
+        if log_root=="default":
+            log_root = os.path.join(ROOT_DIR, 'logs', train_cfg.runner.experiment_name)
+            self.log_dir = os.path.join(log_root, datetime.now().strftime('%b%d_%H-%M-%S') + '_' + train_cfg.runner.run_name)
+        elif log_root is None:
+            self.log_dir = None
+        else:
+            self.log_dir = os.path.join(log_root, datetime.now().strftime('%b%d_%H-%M-%S') + '_' + train_cfg.runner.run_name)
+
+        train_cfg_dict = class_to_dict(train_cfg)
+        if train_cfg.runner.runner_class_name == "OnPolicyRunner":
+            runner = OnPolicyRunner(env, train_cfg_dict, self.log_dir, device=args.rl_device)
+        else:
+            runner_class = eval(train_cfg.runner.runner_class_name)
+            runner = runner_class(env, train_cfg_dict, self.log_dir, device=args.rl_device)
+        #save resume path before creating a new log_dir
+        resume = train_cfg.runner.resume
+        if train_cfg.runner.runner_class_name == "OnPolicyRunner":
+            if resume:
+                # load previously trained model
+                resume_path = get_load_path(log_root, load_run=train_cfg.runner.load_run, checkpoint=train_cfg.runner.checkpoint)
+                print(f"Loading model from: {resume_path}")
+                runner.load(resume_path)
+        return runner, train_cfg
+
+# make global task registry
+task_registry = TaskRegistry()
